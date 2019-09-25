2aa65911b22d1ebd3f13ca82a1109f7168ad932b
kenter <Hjt_830@163.com>
2019-09-25 18:31:43 +0800

    继续聊天页面

---
+++ XOChatModule/Classes/Category/UIImage+XOChatExtension.h
@@ -0,0 +1,16 @@
+//
+//  UIImage+XOChatExtension.h
+//  AFNetworking
+//
+//  Created by kenter on 2019/9/25.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIImage (XOChatExtension)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XOChatModule/Classes/Category/UIImage+XOChatExtension.m
@@ -0,0 +1,12 @@
+//
+//  UIImage+XOChatExtension.m
+//  AFNetworking
+//
+//  Created by kenter on 2019/9/25.
+//
+
+#import "UIImage+XOChatExtension.h"
+
+@implementation UIImage (XOChatExtension)
+
+@end
